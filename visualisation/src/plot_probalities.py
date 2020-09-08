c6aecc44bc7449d35205308779ae8a65e3081c65
amalaise <adrien.malaise@inria.fr>
2020-09-08 14:49:59 +0200

    update by serena

--- visualisation/src/plot_probalities.py
+++ visualisation/src/plot_probalities.py
@@ -28,37 +28,70 @@ class RealTimeBarPlotModule():
 		self.buffer = [[]]
 		self.list_curve = []
 
-		yarp.Network.connect(self.input_port, self.port.getName())
+		if yarp.Network.connect(self.input_port, self.port.getName())== False:
+			print('*** The activity recognition port cannot be found, closing ')
+			self.close()
+			return False
 
 		self.flag_init = 0
 
 
 	def update_plot(self):
-		b_in = self.port.read()
-		data = b_in.toString().split(' ')
 
-		states = []
-		prob = []
+		try:
 
-		for i in range(0, len(data), 2):
-			states.append(data[i])
-			prob.append(float(data[i + 1]))
+			b_in = self.port.read()
+			data = b_in.toString().split(' ')
 
-		x = np.arange(len(states))
+			states = []
+			prob = []
 
-		self.plotData.clear()
+			for i in range(0, len(data), 2):
+				states.append(data[i])
+				prob.append(float(data[i + 1]))
 
-		bg1 = pg.BarGraphItem(x=x, height=prob, width=0.6, brush='r')
+			x = np.arange(len(states))
 
-		self.plotData.addItem(bg1)
-		label_states = dict(enumerate(states))
-		self.plotData.getAxis('bottom').setTicks([label_states.items()])
-		QtGui.QApplication.processEvents()
-		return
+			self.plotData.clear()
+
+			bg1 = pg.BarGraphItem(x=x, height=prob, width=0.6, brush='r')
+
+			self.plotData.addItem(bg1)
+			label_states = dict(enumerate(states))
+			self.plotData.getAxis('bottom').setTicks([label_states.items()])
+			QtGui.QApplication.processEvents()
+			return True
+
+		except KeyboardInterrupt:
+			print('*** Catch a CTRL+C ... closing ')
+			fig.close()
+			return False
+
 
 	def close(self):
+
+		print('*** Closing the ports of this module ***')
+
 		yarp.Network.disconnect(self.input_port, self.port.getName())
 		self.port.close()
+
+		print('*** Finished closing ports ')
+
+		return True
+
+
+	def interruptModule(self):
+
+		print('*** Interrupt the port of this module ***')
+
+		self.port.interrupt()
+
+		print('*** Finished interrupt port ')
+
+		return True
+
+
+####################################################################################""
 
 
 if __name__=="__main__":
@@ -70,10 +103,17 @@ if __name__=="__main__":
 
 	fig = RealTimeBarPlotModule(rf)
 	
-	while(True):
-		try:
-			fig.update_plot()
-		except KeyboardInterrupt:
-			fig.close()
-			break
+	# while(True):
+	# 	try:
+	# 		fig.update_plot()
+	# 	except KeyboardInterrupt:
+	# 		print('*** Catch a CTRL+C ... closing ')
+	# 		fig.close()
+	# 		break
 
+	while(fig.update_plot()):
+		True
+
+	print('Exited after CTRL+C')
+
+	yarp.Network.fini();
